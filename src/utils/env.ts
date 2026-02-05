88ea8c774f14ad3029c29389842cda85a83e0e3f
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2026-02-05 16:18:35 -0500

    updating env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,7 +5,7 @@ const PORT = Number(process.env.PORT) || 3000;
 const DATABASE_URL = process.env.DATABASE_URL ?? "postgresql://proteus:proteus@localhost:5432/proteus";
 const REDIS_URL = process.env.REDIS_URL ?? "redis://localhost:6379";
 
-if (!process.env.DATABASE_URL && process.env.NODE_ENV !== "test") {
+if (!process.env.DATABASE_URL && process.env.NODE_ENV !== "development") {
   throw new Error("DATABASE_URL is required");
 }
 
